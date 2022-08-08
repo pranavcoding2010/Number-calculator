0e69a187cd71abf48d4ae3e0098d8589e6e6d2c7
pranavcoding2010 <110837756+pranavcoding2010@users.noreply.github.com>
2022-08-08 20:45:11 +0530

    Add files via upload

---
+++ Main.py
@@ -0,0 +1,14 @@
+from re import A
+
+
+print('Simple calculator')
+a = int( input("enter the first number="))
+b = int( input("enter the second number="))
+sum=a+b
+subtract=a-b
+product=a*b
+division=a/b
+print("the sum of 2 numbers is",sum)
+print("the subtract of 2 numbers is",subtract)
+print("the product of 2 numbers is",product)
+print("the division of 2 numbers is",division)
